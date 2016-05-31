78f3f43723ac473f46387a7ff88428d5fa6f6f7a
Olivia Hinton and Sam Park <sam@>
2016-05-31 15:19:31 -0700

    fail to toggle images

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  $(".clickable1").click(function() {
+    $("#chick-crown").show();
+    $("#chick-1").hide();
+    $("#chick-crown").fadeToggle();
+    $("#chick-1").fadeToggle();
+  });
+
+}
